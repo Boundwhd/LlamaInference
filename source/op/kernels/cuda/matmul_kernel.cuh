42359900dcf712845e85be2b22aa11e9616e1eb4
Bound-whd <boundwhd@126.com>
2025-03-13 09:37:19 +0800

    2025年3月13日09:37:17

--- source/op/kernels/cuda/matmul_kernel.cuh
+++ source/op/kernels/cuda/matmul_kernel.cuh
@@ -1,6 +1,7 @@
 #ifndef _MATMUL_KERNEL_CUH_
 #define _MATMUL_KERNEL_CUH_
 #include "tensor.h"
+#include "cuda_config.h"
 namespace kernel {
 void matmul_kernel_cu(const tensor::Tensor& input, const tensor::Tensor& weight,
     const tensor::Tensor& output, float scale = 1.f, const CudaConfig* config = nullptr);
